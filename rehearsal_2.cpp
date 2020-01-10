3abd8adaad1e548ef2611967dcce0053b358f09e
phird <59276805+phird@users.noreply.github.com>
2020-01-10 17:35:45 +0700

    Add files via upload

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+using namespace std;
 
+double getX(double x){
+    cout << "Enter X : ";
+    cin  >> x ;
+    }
+
+
+int main(){
+float sum=0,x;
+
+while (x>0)
+{
+    if (x<0)
+    {
+        getX;
+    }else
+    {
+        sum=sum+x;
+    }
+}
+sum=sum+x;
+}
--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
+#include <cmath>
+using namespace std;
 
-//Write sumSqrt() here.
-
-
-int main()
-{
-    double a = sumSqrt(20);
-    double b = sumSqrt(10);
+double sumSqrt(int x){
+int A,i=0;
+A=x-i;    
+double L=(1/(sqrt(A))),ans;
+if(x>0){
+	while(i<x){
+	ans=ans+L;
+	i++;
+}}
+else{
+	return 0;
+}
+}
+ 
+int main(){
+    double a = sumSqrt(-5);
+    double b = sumSqrt(3);
     double c = sumSqrt(5);
     double d = sumSqrt(2);
     double e = sumSqrt(1);
@@ -14,5 +26,4 @@ int main()
     double g = sumSqrt(-1);
     
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
-
 }
